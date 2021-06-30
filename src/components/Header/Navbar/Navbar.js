c5d6259074c1db4c16d532ac4cc3492d7cb8f605
NA-Siddiky <siddiky.academic@gmail.com>
2021-07-01 02:09:04 +0600

    fixing button

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link to="/" className="navbar-brand">Xpeed Hub</Link>
+                    <Link to="/" className="navbar-brand">User Hub</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- src/components/Profile/CreateProfile/CreateProfile.js
+++ src/components/Profile/CreateProfile/CreateProfile.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
+import toast, { Toaster } from 'react-hot-toast';
 
 const CreateProfile = () => {
 
+    const [loader, setLoader] = useState(false);
 
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
 
         console.log(data)
 
-        fetch('http://127.0.0.1:8080/submit_form.php', {
+        fetch('http://localhost/api/submit_form.php', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
         })
             .then(res => res.text())
-            .then(data => console.log(data))
+            .then(data => {
+                setLoader(true);
+                setTimeout(() => {
+                    setLoader(false)
+                    toast('New User created successfully.');
+                    console.log(data)
+                }, 4000)
+
+            })
     };
 
 
@@ -23,6 +33,7 @@ const CreateProfile = () => {
 
     return (
         <div className="my-5">
+            <Toaster />
             <h3 className="my-4">Adding User Profile</h3>
             <form onSubmit={handleSubmit(onSubmit)} className="row">
                 <div className="mb-3">
@@ -45,7 +56,13 @@ const CreateProfile = () => {
                     <input {...register("created_at", { required: true })} type="text" className="form-control" id="created_at" />
                     {errors.created_at && <span>This field is required</span>}
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                {
+                    loader &&
+                    <div class="spinner-border ms-auto text-end" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                }
+                <button type="submit" className="btn btn-primary w-50 ms-auto" disabled={loader}>Submit</button>
             </form>
         </div>
     );
